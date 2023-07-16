bf7f5e7498fad6da01a7086f56a88c8c9c5df57e
BrunoSnts <117349129+BrunoSnts@users.noreply.github.com>
2023-07-17 01:44:38 +1000

    Update EssayBot.py

--- Documents/ezwrites/EssayBot.py
+++ Documents/ezwrites/EssayBot.py
@@ -1,12 +1,1 @@
-import openai
-openai.api_key = "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"
 
-prompt = "Create a 200 word short story which includes many language features"
-response = openai.Completion.create(
-  engine="text-davinci-003",
-  prompt=prompt,
-  max_tokens=50
-)
-
-generated_text = response.choices[0].text.strip()
-print(generated_text)
