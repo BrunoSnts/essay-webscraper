613685f138f6a11973aeeef787751efd3ca121ca
BrunoSnts <117349129+BrunoSnts@users.noreply.github.com>
2023-07-17 02:18:53 +1000

    Update urlScraper.py

--- essayScraper/urlScraper.py
+++ essayScraper/urlScraper.py
@@ -1,20 +1,19 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
-from urllib.request import Request, urlopen
-req = Request(url='https://ivypanda.com/essays/subject/biology/',
+req = Request(url='https://ivypanda.com/essays/subject/biology/',## replace with your own url
     headers={'User-Agent': 'Chrome/114.0.0.0'})
 html_page = urlopen(req)
 soup = BeautifulSoup(html_page, "lxml")
 info = soup.find_all("span", class_="taxonomy-meta__text")
 Length = []
+a = open("url.txt", "a")
 
 x = 0
 for i in info:
     wordcount = int(i.get_text())
     if(x%2 != 0):
-        if(wordcount > 850):
-            print(wordcount)
+        if(wordcount > 850):##replace with your own wordcount
             Length.append(True)
         else:
             Length.append(False)
@@ -27,7 +26,7 @@ for link in soup.findAll('a', class_="article__heading-link"):
 x = 0
 for i in Length:
     if(i == True):
-        print(links[x])
+        a.write(links[x])
     x+=1
 
 
